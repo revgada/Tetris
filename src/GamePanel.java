bd9a0f1449790ef857dc0fd107e33eaf254d620e
Ammar Rahic <ammarrahic@live.com>
2018-01-07 15:32:37 +0000

    Add a constructor to GameWindow and GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,2 +1,14 @@
-public class GamePanel {
+import javax.swing.*;
+import java.util.Timer;
+
+public class GamePanel extends JPanel {
+
+    Engine engine;
+
+    public GamePanel()
+    {
+        engine = new Engine();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(engine, 0, 10);
+    }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -2,7 +2,14 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame
 {
-    private GamePanel panel = new GamePanel();
+    private GamePanel gamePanel = new GamePanel();
 
+    public GameWindow()
+    {
+        this.setVisible (true);
+        this.setSize (700, 900);
+        this.add (gamePanel.engine.board);
+        this.addKeyListener(gamePanel.engine.board);
+    }
 
 }
