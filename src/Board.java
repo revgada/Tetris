3c19a73f918c54f5ba06c457c72f2c4b11dcf4e9
Ammar Rahic <ammarrahic@live.com>
2018-01-07 15:57:41 +0000

    Implement run and nextGameTick methods in the engine class and Initialize Board class

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,21 @@
-public class Board {
+import javax.swing.*;
+import java.awt.event.KeyListener;
+
+public class Board extends JPanel {
+    private int [] [] gameField;
+    private final int gameFieldWidth;
+    private final int gameFieldHeight;
+
+    public Board(int width, int height)
+    {
+//		printArray (tetrisPieces[0]);
+//		printArray (rotate (tetrisPieces[0]));
+
+        this.gameFieldWidth = width;
+        this.gameFieldHeight = height;
+        this.gameField = new int [width] [height];
+
+        this.createPiece();
+    }
+
 }
--- src/Engine.java
+++ src/Engine.java
@@ -1,2 +1,39 @@
-public class Engine {
+import java.util.TimerTask;
+
+public class Engine extends TimerTask {
+
+    Board board;
+    private int tickCounter = 0; //Counts every tick
+    private int tickLimit = 50; //How big tickCounter can get before we drop the piece.
+
+    public Engine()
+    {
+        System.out.println ("Timer started");
+        board = new Board(12, 24);
+    }
+
+    public void nextGameTick()
+    {
+        System.out.println ("tick");
+        board.update();
+        board.repaint();
+    }
+
+
+    @Override
+    public void run ()
+    {
+        tickCounter++;
+
+        if (tickCounter % tickLimit == 0)
+        {
+            nextGameTick();
+        }
+
+        if (board.speedUpGame)
+        {
+            tickLimit = tickLimit * 9 / 10; //reduce delay by 90%.
+            board.speedUpGame = false;
+        }
+    }
 }
