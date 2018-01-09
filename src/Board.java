06a2953fe723536711c8e26026a487c1babdd318
Ammar Rahic <ammarrahic@live.com>
2018-01-09 19:26:25 +0000

    Add KeyListener methods

--- src/Board.java
+++ src/Board.java
@@ -1,16 +1,14 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements KeyListener {
     private int [] [] gameField;
     private final int gameFieldWidth;
     private final int gameFieldHeight;
 
     public Board(int width, int height)
     {
-//		printArray (tetrisPieces[0]);
-//		printArray (rotate (tetrisPieces[0]));
-
         this.gameFieldWidth = width;
         this.gameFieldHeight = height;
         this.gameField = new int [width] [height];
@@ -18,4 +16,33 @@ public class Board extends JPanel {
         this.createPiece();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+        //pass.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            rotateCurrentBlock();
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            pieceDrop();
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            translateCurrentBlock(-1);
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            translateCurrentBlock(1);
+        else if (e.getKeyCode() == KeyEvent.VK_SPACE)
+            while (!pieceDrop());
+        else if (e.getKeyCode() == KeyEvent.VK_SHIFT)
+            holdBlock();
+        this.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        //pass
+    }
 }
